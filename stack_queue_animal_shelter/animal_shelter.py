37aa663a8137c6d82b7eb3c6f169a1bba54f31df
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-06-04 11:51:40 +0000

    animal

--- stack_queue_animal_shelter/animal_shelter.py
+++ stack_queue_animal_shelter/animal_shelter.py
@@ -1,9 +1,11 @@
-from typing import Literal, Union
 from stack_queue_animal_shelter.animal import Animal, Cat, Dog
 
 
 class AnimalShelter:
-    
+    """ A shelter that holds dogs and cats, and returns  first animal that
+      matches the preference"""
+
+
     def __init__(self):
         self.animals = []
 
@@ -16,6 +18,9 @@ class AnimalShelter:
 
     def dequeue(self, pref = None ):
 
+        """Remove the first animal from  shelter that matches 
+        the preference, or the first animal if no preference is given"""
+        
         if pref is None and len(self.animals) > 0:
             return self.animals.pop(0)
 
