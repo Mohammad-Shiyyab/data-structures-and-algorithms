dd0eebb4b51c05f4179caa5bda78697be88da85c
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-07-22 16:12:29 +0000

    hash

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,9 +1,14 @@
 
-class Hashtable():
-    def __init__(self, size = 1024) :
-        self._size = size
-        self._buckets = [None] * size
-        self.key=[]
+from typing import Generic, TypeVar, Optional
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+
+class Hashtable(Generic[K, V]):
+    def __init__(self, size: int = 100) :
+        self._size: int = size
+        self._buckets: list[Optional[list[tuple[K, V]]]] = [None] * size
 
     @property
     def size(self) :
@@ -16,7 +21,7 @@ class Hashtable():
         """
         Insert a new key-value pair into the hashtable
         """
-        hash = self._hash(key)
+        hash: int = self._hash(key)
 
         if (bucket := self._buckets[hash]) is None:
             bucket = []
@@ -28,7 +33,7 @@ class Hashtable():
         """ 
         Get the value associated with a key
         """
-        hash = self._hash(key)
+        hash: int = self._hash(key)
 
         if (bucket := self._buckets[hash]) is None:
             return None
@@ -43,7 +48,7 @@ class Hashtable():
         """
         Check if a key exists in the hashtable
         """
-        hash= self._hash(key)
+        hash: int = self._hash(key)
 
         if (bucket := self._buckets[hash]) is None:
             return False
@@ -58,7 +63,7 @@ class Hashtable():
         """
         Get all the keys in the hashtable
         """
-        keys = []
+        keys: list[K] = []
         for bucket in self._buckets:
             if bucket is not None:
                 for k, _ in bucket:
@@ -71,7 +76,7 @@ class Hashtable():
         Hash a key
         """
         # from ascii to unicode
-        hash = 0
+        hash: int = 0
 
         for char in str(key):
             hash += ord(char)
--- hashtable/tests/test_hashtable.py
+++ hashtable/tests/test_hashtable.py
@@ -1,23 +1,24 @@
 
+from pytest import fixture
 from hashtable.hashtable import Hashtable
 
 
 def test_hashtable_insert_one():
-    hatb = Hashtable()
-    hatb.set('apple', 5)
-    assert hatb.get('apple') == 5
+    ht = Hashtable()
+    ht.set('apple', 5)
+    assert ht.get('apple') == 5
 
 
 def test_hashtable_insert_two():
-    hatb = Hashtable()
-    hatb.set('apple', 5)
-    hatb.set('banana', 6)
-    assert hatb.get('apple') == 5
-    assert hatb.get('banana') == 6
+    ht = Hashtable()
+    ht.set('apple', 5)
+    ht.set('banana', 6)
+    assert ht.get('apple') == 5
+    assert ht.get('banana') == 6
 
 
-
-def hatb():
+@fixture
+def ht():
     hs = Hashtable()
     hs.set('apple', 5)
     hs.set('banana', 6)
@@ -26,35 +27,35 @@ def hatb():
     return hs
 
 
-def test_hashtable_get_none(hatb):
-    assert hatb.get('dinosaur') == None
+def test_hashtable_get_none(ht):
+    assert ht.get('dinosaur') == None
 
 
-def test_hashtable_get_value(hatb):
-    assert hatb.get('apple') == 5
+def test_hashtable_get_value(ht):
+    assert ht.get('apple') == 5
 
 
 def test_hashtable_get_value_collision():
-    hatb = Hashtable(2)
-    hatb.set('apple', 5)
-    hatb.set('banana', 6)
-    hatb.set('carrot', 7)
-    keys = hatb.keys()
+    ht = Hashtable(2)
+    ht.set('apple', 5)
+    ht.set('banana', 6)
+    ht.set('carrot', 7)
+    keys = ht.keys()
     keys.sort()
     assert keys == ['apple', 'banana', 'carrot']
 
 
-def test_hashtable_has_key_true(hatb):
-    assert hatb.has('apple') == True
+def test_hashtable_has_key_true(ht):
+    assert ht.has('apple') == True
 
 
-def test_hashtable_has_key_false(hatb):
-    assert hatb.has('dinosaur') == False
+def test_hashtable_has_key_false(ht):
+    assert ht.has('dinosaur') == False
 
 
-def test_hashtable_key(hatb):
+def test_hashtable_keys(ht):
     expected = ['apple', 'banana', 'carrot']
-    actual = hatb.keys()
+    actual = ht.keys()
     expected.sort()
     actual.sort()
     assert actual == expected
